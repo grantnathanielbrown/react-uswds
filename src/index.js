19c88362f946ba8497e86e32cf54169fe14b8fea
Grant Brown <grant.brown@doveltech.com>
2021-05-10 22:03:09 -0400

    Initial commit and import usdws css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '@trussworks/react-uswds/lib/uswds.css'
+import '@trussworks/react-uswds/lib/index.css'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
